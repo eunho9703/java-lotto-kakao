2b33d94768bd2e244efc794021de8a3de1a071c6
RyooChan <fbcks97@naver.com>
2024-04-02 18:30:06 +0900

    test: 등수 구하는 로직 테스트

---
+++ src/test/java/model/winningLottery/LotteryTest.java
@@ -0,0 +1,50 @@
+package model.winningLottery;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import model.random.LottoNumbers;
+
+import static model.winningLottery.Ranking.FIRST;
+import static model.winningLottery.Ranking.SECOND;
+import static model.winningLottery.Ranking.THIRD;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LotteryTest {
+
+    @Test
+    void 당첨번호와_보너스숫자를_입력받아_몇등인지_구한다_1등() {
+        WinningNumbers winningNumbers = new WinningNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        WinningBonusNumber winningBonusNumber = new WinningBonusNumber(7);
+
+        Lottery lottery = new Lottery(winningNumbers, winningBonusNumber);
+        Ranking rank = lottery.rank(new LottoNumbers(List.of(1, 2, 3, 4, 5, 6)));
+
+        assertThat(rank).isEqualTo(FIRST);
+    }
+
+    @Test
+    void 당첨번호와_보너스숫자를_입력받아_몇등인지_구한다_2등() {
+        WinningNumbers winningNumbers = new WinningNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        WinningBonusNumber winningBonusNumber = new WinningBonusNumber(7);
+
+        Lottery lottery = new Lottery(winningNumbers, winningBonusNumber);
+        Ranking rank = lottery.rank(new LottoNumbers(List.of(1, 2, 7, 4, 5, 6)));
+
+        assertThat(rank).isEqualTo(SECOND);
+    }
+
+    @Test
+    void 당첨번호와_보너스숫자를_입력받아_몇등인지_구한다_3등() {
+        WinningNumbers winningNumbers = new WinningNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        WinningBonusNumber winningBonusNumber = new WinningBonusNumber(7);
+
+        Lottery lottery = new Lottery(winningNumbers, winningBonusNumber);
+        Ranking rank = lottery.rank(new LottoNumbers(List.of(1, 2, 3, 4, 5, 8)));
+
+        assertThat(rank).isEqualTo(THIRD);
+    }
+
+}
