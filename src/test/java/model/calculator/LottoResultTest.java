b49d4aff83ad423c06f45a870f7e2bd146627aea
RyooChan <fbcks97@naver.com>
2024-04-02 18:32:07 +0900

    test: 로또번호 결과 저장 테스트

---
+++ src/test/java/model/calculator/LottoResultTest.java
@@ -0,0 +1,34 @@
+package model.calculator;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import model.random.LottoNumbers;
+import model.winningLottery.Lottery;
+import model.winningLottery.WinningBonusNumber;
+import model.winningLottery.WinningNumbers;
+
+import static java.util.Set.*;
+import static model.winningLottery.Ranking.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoResultTest {
+
+    @Test
+    void 로또_결과_랭킹이_저장된다() {
+        LottoNumbers lottoNumbers = new LottoNumbers(List.of(1,2,3,4,5,6));
+        LottoResult lottoResult = new LottoResult(lottoNumbers);
+
+        WinningNumbers winningNumbers = new WinningNumbers(of(1,2,3,4,5,6));
+        WinningBonusNumber bonusNumber = new WinningBonusNumber(7);
+        Lottery lottery = new Lottery(winningNumbers, bonusNumber);
+
+        LottoResult expectedResult = new LottoResult(lottoNumbers, FIRST);
+
+        lottoResult.saveRanking(lottery);
+
+        assertThat(lottoResult).isEqualTo(expectedResult);
+    }
+}
