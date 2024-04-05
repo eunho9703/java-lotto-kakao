624f08ff3882eb28e318d71070d5307899b765fe
eunho9703 <eunho9703@gmail.com>
2024-04-05 15:05:51 +0900

    refactor: 디렉터리 생성

--- src/main/java/application/LottoApplication.java
+++ src/main/java/application/LottoApplication.java
@@ -1,5 +1,7 @@
 package application;
 
+import controller.LottoController;
+
 public class LottoApplication {
     public static void main(String[] args) {
         LottoController lottoGameController = new LottoController();
