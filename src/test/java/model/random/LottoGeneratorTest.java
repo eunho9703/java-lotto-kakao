78bce9872c22a52bb79c4d13694f7a9098ece50e
eunho9703 <eunho9703@gmail.com>
2024-04-05 14:43:32 +0900

    feat: 수동 구매 로또 Test

--- src/test/java/model/random/LottoGeneratorTest.java
+++ src/test/java/model/random/LottoGeneratorTest.java
@@ -1,17 +1,28 @@
 package model.random;
 
+import model.ManualLotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import model.Amount;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoGeneratorTest {
     @Test
-    void 구매_금액만큼_랜덤_로또번호_6개가_생성된다() {
-        int cost = 6000;
-        Amount amount = Amount.costInput(cost);
+    void 구매_금액만큼_로또가_생성된다() {
+        int entireBudget = 6000;
+        int manualCount=2;
+        int automaticBudget = entireBudget-manualCount*1000;
 
-        LottoGenerator lottoGenerator = LottoGenerator.generate(amount);
-        Assertions.assertThat(lottoGenerator.getWholeLottoSize()).isEqualTo(6);
+        String manualString ="1,2,3,4,5,6 \n 1,2,3,4,5,6";
+        List<String> manualList = Arrays.asList(manualString.split("\n"));
+
+        Amount amount = Amount.amountInput(automaticBudget);
+
+        LottoGenerator lottoGenerator = LottoGenerator.generate(manualList,amount);
+        Assertions.assertThat(lottoGenerator.getEntireLottoSize()).isEqualTo(6);
     }
+
 }
